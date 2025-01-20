1fa450785f53cf0b729bf6f9f2c3ff848794e5fb
Steven Dake <steven.dake@gmail.com>
2025-01-19 18:21:44 -0700

    Align with upstream brew version while retaining customized build flags.

--- Formula/rsync.rb
+++ Formula/rsync.rb
@@ -1,51 +1,70 @@
 class Rsync < Formula
-  desc "Utility for fast incremental file transfer"
+  desc "Utility that provides fast incremental file transfer"
   homepage "https://rsync.samba.org/"
-  url "https://download.samba.org/pub/rsync/rsync-3.4.1.tar.gz"
+  url "https://rsync.samba.org/ftp/rsync/rsync-3.4.1.tar.gz"
+  mirror "https://mirrors.kernel.org/gentoo/distfiles/rsync-3.4.1.tar.gz"
+  mirror "https://www.mirrorservice.org/sites/rsync.samba.org/rsync-3.4.1.tar.gz"
   sha256 "2924bcb3a1ed8b551fc101f740b9f0fe0a202b115027647cf69850d65fd88c52"
+  license "GPL-3.0-or-later"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
 
-  depends_on "gettext"         # Internationalization support
-#  depends_on "libiconv"        # Character encoding
-  depends_on "libiconv" if OS.mac?  # Character encoding (macOS specific)
-  depends_on "libacl" if OS.linux?  # ACL support (Linux specific)
-  depends_on "zlib"            # Compression library
-  depends_on "popt"            # Command-line option parsing
-  depends_on "lz4"             # LZ4 compression
-  depends_on "zstd"            # Zstandard compression
-  depends_on "bzip2"           # Bzip2 compression
-  depends_on "xz"              # XZ compression
-  depends_on "wolfssl"         # SSL/TLS library for secure transfers
+  depends_on "gettext"
+  depends_on "libiconv" if OS.mac?
+  depends_on "libacl" if OS.linux?
 
-  def install
-    args = [
+  depends_on "lz4"
+  depends_on "wolfssl"
+  depends_on "popt"
+  depends_on "xxhash"
+  depends_on "xz"
+  depends_on "zlib"
+  depends_on "zstd"
+  depends_on "bzip2"
+
+  uses_from_macos "zlib"
+
+   def install
+    args = %W[
       "--prefix=#{prefix}",
-      "--enable-acl-support",       # Enable Access Control Lists
-      "--enable-xattr-support",    # Enable extended attributes
-      "--enable-fileflags",        # Enable file flags
-      "--enable-crtimes",          # Enable creation times
-      "--enable-iconv",            # Enable character encoding conversion
-      "--enable-ipv6",             # Enable IPv6 support
-      "--enable-debug",            # Include debug symbols
-      "--with-included-popt",      # Use included POPT library
-      "--with-included-zlib",      # Use included Zlib
-      "--with-wolfssl"             # Use WolfSSL for secure communication
+      "--with-rsyncd-conf=#{etc}/rsyncd.conf",
+      "--with-included-popt=no",
+      "--with-included-zlib=no",
+      "--enable-acl-support",
+      "--enable-xattr-support",
+      "--enable-fileflags",
+      "--enable-crtimes",
+      "--enable-iconv",
+      "--enable-ipv6",
+      "--enable-debug",
+      "--with-included-popt",
+      "--with-included-zlib",
+      "--with-wolfssl"
     ]
 
-    # Specify compression library paths
     args << "--with-lz4=#{Formula["lz4"].opt_prefix}"
     args << "--with-zstd=#{Formula["zstd"].opt_prefix}"
     args << "--with-bzip2=#{Formula["bzip2"].opt_prefix}"
     args << "--with-xz=#{Formula["xz"].opt_prefix}"
 
-    system "./configure", *args
+    system "./configure", *args, *std_configure_args
+    system "make"
     system "make", "install"
   end
 
   test do
-    # Test if the installed binary runs correctly
-    assert_match version.to_s, shell_output("#{bin}/rsync --version")
-    system "#{bin}/rsync", "--version"
-    
+    mkdir "a"
+    mkdir "b"
+
+    ["foo\n", "bar\n", "baz\n"].map.with_index do |s, i|
+      (testpath/"a/#{i + 1}.txt").write s
+    end
+
+    system bin/"rsync", "--archive --recusive --version --times", testpath/"a/", testpath/"b/"
+
+    (1..3).each do |i|
+      assert_equal (testpath/"a/#{i}.txt").read, (testpath/"b/#{i}.txt").read
+    end
   end
 end
