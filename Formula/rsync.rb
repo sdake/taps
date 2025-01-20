a55b4c3d82acd963fccfae7dcf4a362cce2f8b13
Steven Dake <steven.dake@gmail.com>
2025-01-19 23:56:09 -0700

    Update.

--- Formula/rsync.rb
+++ Formula/rsync.rb
@@ -51,6 +51,10 @@ class Rsync < Formula
       "--with-xz"
     ]
 
+    args << "--with-zstd=#{Formula["zstd"].opt_prefix}"
+    args << "--with-bzip2=#{Formula["bzip2"].opt_prefix}"
+    args << "--with-xz=#{Formula["xz"].opt_prefix}"
+
     system "./configure", *args || (raise "Configure failed. Check config.log.")
 
     system "make"
